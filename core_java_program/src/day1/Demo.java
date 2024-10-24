f466cc20e9436f09ec2564fc2fe5a4cc504878d0
saravanan <saravanan@LAPTOP-O4S1EA5S>
2024-10-24 16:04:01 +0530

    add to java basics

---
+++ core_java_program/src/day1/Demo.java
@@ -0,0 +1,15 @@
+package day1;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+"is:"+c);
+	}
+
+}
